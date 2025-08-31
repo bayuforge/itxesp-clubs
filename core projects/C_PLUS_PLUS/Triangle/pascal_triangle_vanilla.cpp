6e4435fb62e57181ec58c1b4a7b704fd16bf24e0
bayuforge <bayuanataw@gmail.com>
2025-08-31 12:00:07 +0800

    Updated pascal_triangle_vanilla.cpp
    This is an updated version of the vanilla version of pascal triangle script.
    
    Published & Authored by bayuforge.
    31st August 2025.

--- core projects/C_PLUS_PLUS/Triangle/pascal_triangle_vanilla.cpp
+++ core projects/C_PLUS_PLUS/Triangle/pascal_triangle_vanilla.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+using std::cout, std::cin;
 
 /* PASCAL EQUATION
     new_val = (current_val * numerator) / denominator
@@ -7,10 +7,14 @@ using namespace std;
     when: numerator = row - cln
           denominator = cln + 1
           set -> current_val = new_val for after compute pascal equation
-*\
+          
+          
+    DEV NOTE
+    After pascal length row > 4, Then add 2 spacing
+*/
 
 int i; // count for n iteration
-int s1, s2; // count for n space string before and after c
+int s1; // count for n space string before and after c
 int c; // c is main content, the numbers
 int length; // count for the length of the triangle input by users
 
@@ -19,11 +23,23 @@ int cln;  // check for collumn pos for every n iteration
 int current_value, new_value;  // check current value for pascal to work
 int numerator, denominator; // part of computing pascal equation
 
-string space = " ";  // space character
+char space = ' ';  // space character
+
+void error_message();
+
+int grouping(int length){
+    if (length % 4 == 0){
+        cout << space; 
+        return 0;
+    }
+    else{
+        return 0;
+    }
+}
 
 int main() 
 {
-    cout << "Enter pascal length: ";
+    cout << "Enter pascal row: ";
     cin >> length;
     
     for (i = 0; i <= length; i++)
@@ -31,12 +47,25 @@ int main()
         current_value = 1;
         cln = 0;
         
+        grouping(i);
+        
         for (s1 = length; s1 > i; s1--)
         {
-            cout << space;
+            if (s1 % 4 == 0 && s1 > 0){
+                cout << space << space;  
+            }
+            
+            else{
+                cout << space;
+            }
         }
-        
-        cout << space << 1 << space;
+ 
+        if (s1 == 0){
+            cout << 1;
+        }
+        else{
+            cout << space << 1 << space;   
+        }
         
         for (c = 0; c < i; c++)
         {
@@ -47,18 +76,20 @@ int main()
             cout << new_value << space;
             cln++;
             current_value = new_value;
-               
         }
         
-        for (s2 = length; s2 > i; s2--)
-        {
-            cout << space; 
-        }
-        
-        cout << endl;
-        
+        cout << "\n";
         row++;
     }
-
+    
+    if (length > 9){
+        error_message();
+    }
+    
     return 0;
+}
+
+void error_message(){
+    cout << "\n\n\n" << "ERROR_MESSAGE: \n" << "WHEN LENGTH > 9 THE TRIANGLE BREAKS....";
+    cout << "\nthere is not yet found efficient solution to solve this,\nPerhaps in the next timeline?";
 }
